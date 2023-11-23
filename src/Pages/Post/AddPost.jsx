ef3caea55dc9502ad9e2b7a638359c2d3b145506
YourmixJNR <radmakson@gmail.com>
2023-11-23 16:02:32 +0100

    uuid init

--- src/Pages/Post/AddPost.jsx
+++ src/Pages/Post/AddPost.jsx
@@ -1,6 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { supabase } from "../../supabaseClient";
+import { v4 as uuid } from "uuid";
 
 const AddPost = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [content, setContent] = useState("");
+
+  let navigate = useNavigate()
+
+  const addBlog = async ({ title, description, content }) => {
+    try {
+      const update = {
+        id: uuid,
+        title: title,
+        description: description,
+        content: content,
+      };
+
+      const { error } = await supabase
+        .from("blog")
+        .insert(update)
+        .then(navigate("/"));
+
+        if(error) {
+            throw error
+        }
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <header className="masthead">
@@ -22,19 +54,31 @@ const AddPost = () => {
                 <div>
                   <div className="mb-3 pb-1">
                     <label className="form-label px-0">Post title</label>
-                    <input className="form-control" />
+                    <input
+                      className="form-control"
+                      value={title}
+                      onChange={(e) => setTitle(e.target.value)}
+                    />
                   </div>
                 </div>
                 <div>
                   <div className="mb-3 pb-1">
                     <label className="form-label px-0">Short description</label>
-                    <input className="form-control" />
+                    <input
+                      className="form-control"
+                      value={description}
+                      onChange={(e) => setDescription(e.target.value)}
+                    />
                   </div>
                 </div>
                 <div>
                   <div className="mb-3 pb-1">
                     <label className="form-label px-0">Post content</label>
-                    <textarea className="form-control" />
+                    <textarea
+                      className="form-control"
+                      value={content}
+                      onChange={(e) => setContent(e.target.value)}
+                    />
                   </div>
                 </div>
                 <div>
@@ -46,6 +90,7 @@ const AddPost = () => {
                 <button
                   className="btn btn-light shadow btn-sm mb-2"
                   type="submit"
+                  onClick={() => addBlog(title, description, content)}
                 >
                   Add
                 </button>
